11b64adb3e09a0b18053ddbbae30c82718b95600
SILLA GANESH <sillaganesh@SILLAs-MacBook-Pro.local>
2024-02-01 18:34:20 +0530

    added

--- registration.js
+++ registration.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   
       // Validate password
-      if (!password.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)) {
+      if (!password.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/)) {
         alert('Password should be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character');
         return;
       }
